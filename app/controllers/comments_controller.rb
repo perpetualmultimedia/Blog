b7bd15758ab353861fefdb56a742ee0d25734954
Colton Hagan <me@perpetualsite.com>
2016-01-02 02:04:33 -0700

    Added new functionality Fucking heroku

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "coltonhagan", password: "Charitydoes8", except: [:index, :show]
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
